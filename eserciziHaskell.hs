1a6f75796b806946f569789f1abe9ddce88fe63c
Riccardo Belliato <belliatoriccardo@gmail.com>
2021-05-17 12:03:01 +0200

    altri esercizi

--- eserciziHaskell.hs
+++ eserciziHaskell.hs
@@ -9,12 +9,9 @@ binomial n k
     | otherwise = binomial (n-1) k + binomial (n-1) (k-1)
 
 -- Esercizio Liste.1
-removeEven xs = reverse (removeEvenAux xs 1 [])
-    where
-        removeEvenAux [] _ acc = acc
-        removeEvenAux (x:xs) i acc
-            | odd i    = removeEvenAux xs (i+1) (x:acc)
-            | otherwise = removeEvenAux xs (i+1) acc
+removeEven [] = []
+removeEven [x] = [x]
+removeEven (x:y:xs) = x : removeEven xs
 
 -- Esercizio Liste.4
 minOdd xs = minOddAux oddListWithoutFirstTwo firstMinTuple
@@ -28,11 +25,6 @@ minOdd xs = minOddAux oddListWithoutFirstTwo firstMinTuple
             | (x >= a) && (x < b)   = minOddAux xs (a, x)
             | otherwise             = minOddAux xs (a, b)
 
-
--- Esercizio Liste.5
-tupleList [] = []
-tupleList (x:xs) = (x, sum xs) : tupleList xs
-
 -- Esercizio Matrici.1
 matrixDim m  = matrixDimAux (tail m) 1 (length (head  m))
     where
@@ -43,7 +35,7 @@ matrixDim m  = matrixDimAux (tail m) 1 (length (head  m))
 
 -- Esercizio Matrici.8
 convergent m r = convergentAux m r 0
-    where 
+    where
         convergentAux [] r i      = True
         convergentAux (row:m) r i = convergentAux m r (i+1) && (abs(sumRow row 0) < r)
             where
@@ -51,3 +43,59 @@ convergent m r = convergentAux m r 0
                 sumRow (x:xs) index
                     | index == i    = sumRow xs (index + 1)
                     | otherwise     = x + sumRow xs (index + 1)
+
+data BTree a = NullBTree | BTree a (BTree a) (BTree a)
+    deriving (Eq, Ord, Read, Show)
+
+-- Esercizio Alberi.4
+bstElem NullBTree _ = False
+bstElem (BTree n left right) x
+    | n == x    = True
+    | x < n     = bstElem left x
+    | otherwise = bstElem right x
+infixl 9 `bstElem`
+
+-- Esercizio Alberi.9
+data Node a = Leaf | Node a Integer
+    deriving (Eq, Ord, Read, Show)
+
+annotate NullBTree = BTree Leaf NullBTree NullBTree
+annotate (BTree x leftSon rightSon) = BTree (Node x nodeHeight) newLeftSon newRightSon
+    where
+        newLeftSon = annotate leftSon
+        newRightSon = annotate rightSon
+        nodeHeight = 1 + max (getNodeheight newLeftSon) (getNodeheight newRightSon)
+        getNodeheight (BTree Leaf l r) = 0
+        getNodeheight (BTree (Node x h) l r) = h
+
+fold :: (Ord a) => (a -> b -> b -> b) -> b -> BTree a -> b
+fold _ z NullBTree = z
+fold f z (BTree x l r) = f x (fold f z l) (fold f z r)
+
+-- Esercizio Alberi.14
+treeHeight NullBTree = 0
+treeHeight tree = fold (\ val lacc racc -> 1 + max lacc racc) 0 tree
+
+
+data Tree a = NullTree | Tree a [Tree a]
+    deriving (Eq, Show)
+
+-- Esercizio Alberi Generici.1
+treefold :: (Eq a, Show a) => (a -> [b] -> b) -> b -> Tree a -> b
+treefold f z NullTree = z
+treefold f z (Tree val sons) = f val [treefold f z x | x <- sons]
+
+-- Esercizio Alberi Generici.2
+height :: (Num p, Show a, Ord p, Eq a) => Tree a -> p
+height = treefold (\ x sonsHeight -> 1 + maxHeight sonsHeight) (-1)
+    where
+        maxHeight [] = -1
+        maxHeight (x:xs) = max x (maxHeight xs)
+
+-- Esercizio Alberi Generici.4
+--treefoldr :: (Eq a, Show a) => (a -> b -> c) -> c -> (c -> b -> b) -> b -> Tree a -> c
+--treefoldr _ z _ _ NullTree = z
+--treefoldr f z g k (Tree val []) = treefoldr f z g k 
+--treefoldr f z g k (Tree val (x:sons)) = f val (g (treefoldr f z g k x) k)
+
+-- Esercizio 
