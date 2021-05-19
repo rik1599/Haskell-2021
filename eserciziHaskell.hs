6293c74bc529a6a87dbb5b0b33256cc4a42dd305
Riccardo Belliato <belliatoriccardo@gmail.com>
2021-05-19 17:55:11 +0200

    esercizi haskell

--- eserciziHaskell.hs
+++ eserciziHaskell.hs
@@ -46,6 +46,24 @@ convergent m r = convergentAux m r 0
                     | index == i    = sumRow xs (index + 1)
                     | otherwise     = x + sumRow xs (index + 1)
 
+-- Esercizio 9
+transpose [] = []
+transpose ([]:xss) = transpose xss
+transpose ((x:xs): xss) = (x : [h | (h:_) <- xss]) : transpose (xs : [t | (_:t) <- xss])
+
+-- Esercizio 11
+matrixProd m1 m2 = matrixProdAux m1 tM2
+    where
+        tM2 = transpose m2
+        matrixProdAux [] _ = []
+        matrixProdAux (row:m1) m2 = calculateRow row m2 : matrixProdAux m1 m2
+            where
+                calculateRow row [] = []
+                calculateRow row (col:m2) = calculateElement row col 0: calculateRow row m2
+                    where
+                        calculateElement [] [] acc = acc
+                        calculateElement (x:row) (y:col) acc = calculateElement row col ((x*y) + acc)
+
 -- ALBERI
 data BTree a = NullBTree | BTree a (BTree a) (BTree a)
     deriving (Eq, Ord, Read, Show)
@@ -58,18 +76,20 @@ bstElem (BTree n left right) x
     | otherwise = bstElem right x
 infixl 9 `bstElem`
 
--- Esercizio 9
-data Node a = Leaf | Node a Integer
-    deriving (Eq, Ord, Read, Show)
+-- Esercizio 6
+bst2List NullBTree = []
+bst2List (BTree x l r) = bst2List l ++ (x : bst2List r)
 
-annotate NullBTree = BTree Leaf NullBTree NullBTree
-annotate (BTree x leftSon rightSon) = BTree (Node x nodeHeight) newLeftSon newRightSon
+-- Esercizio 9
+annotate :: (Num b, Ord b) => BTree a -> BTree (a, b)
+annotate NullBTree = NullBTree
+annotate (BTree x leftSon rightSon) = BTree (x, nodeHeight) newLeftSon newRightSon
     where
         newLeftSon = annotate leftSon
         newRightSon = annotate rightSon
         nodeHeight = 1 + max (getNodeheight newLeftSon) (getNodeheight newRightSon)
-        getNodeheight (BTree Leaf l r) = 0
-        getNodeheight (BTree (Node x h) l r) = h
+        getNodeheight NullBTree = 0
+        getNodeheight (BTree (x, h) l r) = h
 
 fold :: (Ord a) => (a -> b -> b -> b) -> b -> BTree a -> b
 fold _ z NullBTree = z
@@ -85,7 +105,7 @@ data Tree a = NullTree | Tree a [Tree a]
 
 -- Esercizio 1
 treefold :: (Eq a, Show a) => (a -> [b] -> b) -> b -> Tree a -> b
-treefold f z NullTree = z
+treefold _ z NullTree = z
 treefold f z (Tree val sons) = f val [treefold f z x | x <- sons]
 
 -- Esercizio 2
@@ -95,14 +115,13 @@ height = treefold (\ x sonsHeight -> 1 + maxHeight sonsHeight) (-1)
         maxHeight [] = -1
         maxHeight (x:xs) = max x (maxHeight xs)
 
+-- Esercizio 3
+simplify :: (Eq a, Show a) => Tree a -> Tree a
+simplify NullTree = NullTree
+simplify tree = treefold (\ val sons -> Tree val [x | x <- sons, x /= NullTree]) NullTree tree
+
 -- Esercizio 4
---treefoldr :: (Eq a, Show a) => (a -> b -> c) -> c -> (c -> b -> b) -> b -> Tree a -> c
---treefoldr _ z _ _ NullTree = z
---treefoldr f z g k (Tree val []) = treefoldr f z g k 
---treefoldr f z g k (Tree val (x:sons)) = f val (g (treefoldr f z g k x) k)
+treefoldr :: (Eq a, Show a) => (a -> b -> c) -> c -> (c -> b -> b) -> b -> Tree a -> c
+treefoldr _ z _ _ NullTree = z
 
--- QUAD TREES
-data QT a = C a | Q (QT a) (QT a) (QT a) (QT a)
-    deriving (Eq, Show)
-
--- Esercizio 1
+-- Esercizio 10
